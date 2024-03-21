8c3d36a8220c05b87fc3ef685ee887ed63d2e23f
“Denis <deni.kaiju@gmail.com>
2024-03-21 14:24:24 +0100

    Project Update

--- support/support/urls.py
+++ support/support/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import exchange_rate_view
 
 urlpatterns = [
-    path('exchange-rate/', exchange_rate_view),
+    path("exchange-rate/", exchange_rate_view),
 ]
